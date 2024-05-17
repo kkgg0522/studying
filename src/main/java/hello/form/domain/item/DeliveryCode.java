6e79c84bde9ac802e8832a8c11e28669ae8a5596
wonhyeong <kkgg0523@gmail.com>
2024-05-17 23:26:10 +0900

    form

---
+++ src/main/java/hello/form/domain/item/DeliveryCode.java
@@ -0,0 +1,11 @@
+package hello.form.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+    private String code;
+    private String displayName;
+}
